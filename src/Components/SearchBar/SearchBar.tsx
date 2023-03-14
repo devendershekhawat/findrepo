ff7b905a14be3eb66c1cf3ba1b18f6dd05f835dc
Devender Singh Shekhawat <devender.shekhawat0296@gmail.com>
2023-03-14 10:00:06 +0530

    initialize cra app with basic styles and antd

---
+++ src/Components/SearchBar/SearchBar.tsx
@@ -0,0 +1,11 @@
+import { Input } from 'antd';
+
+function SearchBar() {
+    return (
+        <div className='SearchBar'>
+            <Input />
+        </div>
+    );
+}
+
+export default SearchBar;
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { ConfigProvider, theme, Layout } from 'antd';
+import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import 'antd/dist/reset.css';
+
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <ConfigProvider theme={{
+            algorithm: theme.darkAlgorithm,
+        }}>
+            <Layout style={{ minHeight: '100vh' }}>
+                <Layout.Content>
+                    <App />
+                </Layout.Content>
+            </Layout>
+        </ConfigProvider>
+    </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
